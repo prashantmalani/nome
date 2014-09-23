8ea522505579a0b011f1b39a9b707941a80ab320
Prashant Malani <p.malani@gmail.com>
2014-09-23 01:24:31 -0700

    Add a few buttons for tempo
    Current temp is shown in an EditText.
    Need to change this to TextView in some way.

--- wear/src/main/java/com/apps/pmalani/nome/WearMainActivity.java
+++ wear/src/main/java/com/apps/pmalani/nome/WearMainActivity.java
@@ -4,14 +4,21 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class WearMainActivity extends Activity {
 
     private TextView mTextView;
+    private int mCurrentTempo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Initialize tempo to 60 bpm
+        mCurrentTempo = 60;
         setContentView(R.layout.activity_wear_main);
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
@@ -20,5 +27,19 @@ public class WearMainActivity extends Activity {
                 mTextView = (TextView) stub.findViewById(R.id.text);
             }
         });
+    }
+
+    public void handleClick(View v) {
+        switch(v.getId()) {
+            case R.id.temp_decrease_button:
+                mCurrentTempo--;
+                break;
+            case R.id.tempo_increase_button:
+                mCurrentTempo++;
+            default:
+                break;
+        }
+        EditText tempo_text = (EditText)findViewById(R.id.cur_tempo_text);
+        tempo_text.setText("" + mCurrentTempo);
     }
 }
