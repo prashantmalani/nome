aed88c22da344895b3f6c160394fe5e290b23d3a
Prashant Malani <p.malani@gmail.com>
2014-09-23 23:12:00 -0700

    Start periodic task
    We an update the tempo on the fly.
    One tiny vibration per beat.
    Need to figure out a better way to signal end of the measure.

--- wear/src/main/java/com/apps/pmalani/nome/WearMainActivity.java
+++ wear/src/main/java/com/apps/pmalani/nome/WearMainActivity.java
@@ -2,18 +2,43 @@ package com.apps.pmalani.nome;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Vibrator;
 import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
 
 
 public class WearMainActivity extends Activity {
 
     private TextView mTextView;
+
+    private static int MILLISECONDS_PER_MIN = 60 * 1000;
+
+    private static int VIBRATE_LENGTH_NORMAL = 5;
+    private static int VIBRATE_LENGTH_COMPLETE = 20;
+
+    // Current beats per minute value
     private int mCurrentTempo;
+
+    // Time interval in milliseconds
+    private int mTimeInterval;
+
+    // NOTE: Currently we only support quarter notes per measure
+    private int mNotesPerMeasure = 4;
+
+    private int mCurrentNote = 0;
+
     private Vibrator mVibrator;
+
+    private boolean mCurrentlyRunning = false;
+
+    private Handler mPeriodicHandler;
+
+    private void updateParams() {
+        return;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +46,10 @@ public class WearMainActivity extends Activity {
 
         // Initialize tempo to 60 bpm
         mCurrentTempo = 60;
+        mTimeInterval = MILLISECONDS_PER_MIN / mCurrentTempo;
         mVibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
+        mPeriodicHandler = new Handler();
+
         setContentView(R.layout.activity_wear_main);
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
@@ -32,22 +60,52 @@ public class WearMainActivity extends Activity {
         });
     }
 
-    public void handleClick(View v) {
-        switch(v.getId()) {
-            case R.id.temp_decrease_button:
-                mCurrentTempo--;
-                break;
-            case R.id.tempo_increase_button:
-                mCurrentTempo++;
-            default:
-                break;
+    public void handleTempoClick(View v) {
+        synchronized (this) {
+            switch (v.getId()) {
+                case R.id.temp_decrease_button:
+                    mCurrentTempo--;
+                    break;
+                case R.id.tempo_increase_button:
+                    mCurrentTempo++;
+                default:
+                    break;
+            }
+            TextView tempo_text = (TextView) findViewById(R.id.cur_tempo_text);
+            tempo_text.setText("" + mCurrentTempo);
+
+            mTimeInterval = MILLISECONDS_PER_MIN / mCurrentTempo;
+
         }
-        EditText tempo_text = (EditText)findViewById(R.id.cur_tempo_text);
-        tempo_text.setText("" + mCurrentTempo);
+    }
 
-        // Sample to show how the vibration works
-        mVibrator.vibrate(20);
+    Runnable mBeatsTask = new Runnable() {
+        @Override
+        public void run() {
+            // TODO: Need to worry about synchronization of the member variables here
+            mCurrentNote = (mCurrentNote + 1) % mNotesPerMeasure;
+            if (mCurrentNote == 0) {
+                //End of Bar, so vibrate longer
+                mVibrator.vibrate(VIBRATE_LENGTH_COMPLETE);
+            } else {
+                mVibrator.vibrate(VIBRATE_LENGTH_NORMAL);
+            }
+            mVibrator.vibrate(VIBRATE_LENGTH_NORMAL);
+            mPeriodicHandler.postDelayed(this, mTimeInterval);
+        }
+    };
 
-        // TODO : Recalculate time between beats here!
+    public void startStopClick(View v) {
+        Button startButton = (Button)findViewById(R.id.startButton);
+
+        if (mCurrentlyRunning == false) {
+            mCurrentlyRunning = true;
+            mBeatsTask.run();
+            startButton.setText("Stop");
+        } else {
+            startButton.setText("Start");
+            mPeriodicHandler.removeCallbacks(mBeatsTask);
+            mCurrentlyRunning = false;
+        }
     }
 }
